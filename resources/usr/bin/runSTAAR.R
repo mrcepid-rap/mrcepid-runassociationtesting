340dd37dffb0767e2efeacce16402275ef53cc30
Eugene Gardner <egardner@umaryland.edu>
2021-11-09 16:13:20 +0000

    Commented runSTAAR.R

--- resources/usr/bin/runSTAAR.R
+++ resources/usr/bin/runSTAAR.R
@@ -1,11 +1,19 @@
 #!/usr/bin/Rscript --vanilla
 
+# Load required libraries
 library(Matrix)
 library(STAAR)
 library(data.table)
 
+# Read in inputs. They are:
+# 1. [1] A sparse matrix[i][j] where i = samples and j = variants
+# 2. [2] Variants file that has the same length as j with variant IDs, positions, and MAF
+# 3. [3] Annotations file that has the same length as j with variant gene information
+# 4. [4] Filtered covariates + phenotypes file
+# 5. [5] Phenotype name. Must be identical to some column in [4]
+# 6. [6] File output prefix
+# 7. [7] Boolean flag for if our phenotype is binary
 args = commandArgs(trailingOnly = T)
-
 matrix_file = args[1]
 variants_file = args[2]
 annotations_file = args[3]
@@ -33,24 +41,24 @@ variants <- merge(variants, annotations, by = "chrID")
 data_for_STAAR <- fread(covariates_file)
 
 # Build data table for STAAR
-# Remember! Not necessary to exclude individuals as the covariate and phenotype files take care of that
+# Remember! Not necessary to exclude individuals from the covariate / phenotype file as we take care of that in the main applet code
 data.cols <- c("FID", "age", "batch", "sex", paste0("PC", seq(1,10)), pheno_name)
 data_for_STAAR <- data_for_STAAR[,..data.cols]
 data_for_STAAR[,FID:=as.character(FID)]
 
-# Trim the genotypes down to those in the covar file:
-poss <- rownames(genotypes)
-poss <- poss[poss %in% data_for_STAAR[,FID]]
-genotypes <- genotypes[poss, 1:ncol(genotypes)]
-data_for_STAAR <- data_for_STAAR[FID %in% poss]
+# Trim the genotypes down to individuals included in the covariate file:
+poss <- rownames(genotypes) # this gets possible individuals in the genotype matrix
+poss <- poss[poss %in% data_for_STAAR[,FID]] # Subset to those samples that exist in the covariate / pheno file
+genotypes <- genotypes[poss, 1:ncol(genotypes)] # And then use that list to pare down the genotype matrix
+data_for_STAAR <- data_for_STAAR[FID %in% poss] # Just make sure everything is the same between the genotype matrix and covariate / pheno file
+genotypes <- genotypes[data_for_STAAR[,FID],1:ncol(genotypes)] # This sorts the genotype matrix so it lines up with the covariate / pheno data frame:
 
-# Sort the matrix so it lines up with the data frame:
-genotypes <- genotypes[data_for_STAAR[,FID],1:ncol(genotypes)]
-
-# Fit the null model:
+# Fit the null model for STAAR:
+# These lines just autoformat our formula for association testing
 covariates <- c("age","sex","batch",paste0("PC",seq(1,10)))
 cov.string <- paste(covariates, collapse=" + ")
 formated.formula <- as.formula(paste(pheno_name, cov.string,sep=" ~ "))
+# And run either a linear or logistic model according to is_binary
 if (is_binary) {
   obj_nullmodel <- fit_null_glm(formated.formula, data=data_for_STAAR, family="binomial")
 } else {
@@ -60,20 +68,26 @@ if (is_binary) {
 poss.genes <- unique(variants[,GENEID])
 gene.results <- data.table(geneID=poss.genes, n.samps=nrow(genotypes))
 
+# This function just takes one gene and runs it through STAAR
 staar.gene <- function(gene) {
-    
+  # Grabs all columns in the genotype matrix that are for the given gene
   current_GENE <- Matrix(genotypes[,variants[GENEID == gene, rownum]])
+
+  # Only run STAAR if there is greater than one non-ref genotype
   if (sum(current_GENE) > 1) {
     staar_result <- STAAR(genotype = current_GENE, obj_nullmodel = obj_nullmodel, rare_maf_cutoff = 1)
     return(list(staar_result$results_STAAR_O, staar_result$num_variant, staar_result$cMAC))
   } else {
+    # Else just return NaN to indicate the test was not run
     return(list(NaN, 0, 0))
   }
   
 }
 
+# This just uses data.frame functionality to run the function staar.gene on each row of the table (i.e. each gene)
 gene.results[,c("staar.O.p","n.var","cMAC"):=staar.gene(geneID),by=1:nrow(gene.results)]
 
+# And write the final output table
 fwrite(gene.results, paste0("/test/", filename_prefix, ".STAAR_results.tsv"), sep = "\t", quote = F, row.names = F, col.names = T, na = "NaN")
 
 
